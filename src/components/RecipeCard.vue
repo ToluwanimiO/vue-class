adc6ffad68df7406d73bb0df2cad5dc7c1d41b9a
ToluwanimiO <toluwanimiosuolale@gmail.com>
2024-06-03 11:51:00 +0100

    emit class

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -3,6 +3,9 @@
   <!-- {{ recipedetails }} -->
   <div class="col-md-3">
     <img :src="recipedetails.image" width="100" alt="" />
+    <!-- <button @click="emit('updateFavourites')">Send Item to Parent</button> -->
+    <i :class="`${added?'fa-solid':'fa-regular'} text-danger fa-heart`" @click="toggleFave"></i>
+
     <!-- Button trigger modal -->
     <button
       type="button"
@@ -26,6 +29,13 @@
   </div>
 </template>
 <script setup>
+import { ref } from 'vue';
 import RecipeModal from './RecipeModal.vue';
 defineProps(['recipedetails'])
+const emit = defineEmits(['updateFavourites','changeName'])
+const added = ref(false)
+const toggleFave = ()=>{
+  emit('updateFavourites',added.value)
+  added.value = !added.value
+}
 </script>
--- src/views/RecipePage.vue
+++ src/views/RecipePage.vue
@@ -1,21 +1,37 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRecipeStore } from '@/store/recipeStore'
 import RecipeCard from '../components/RecipeCard.vue'
 import { storeToRefs } from 'pinia'
 
 const store = useRecipeStore()
 const { recipes } = storeToRefs(store)
+const count = ref(0)
 
 onMounted(() => {
   store.getAllRecipes()
 })
+const updateMe = (me) => {
+  console.log(me)
+  console.log('from child')
+  if (!me) {
+    count.value++
+  } else {
+    count.value--
+  }
+}
 </script>
 <template>
   <h1>Recipes</h1>
+  Count {{ count }}
   <div class="container">
     <div class="row">
-        <RecipeCard v-for="(recipe, index) in recipes" :key="index" :recipedetails="recipe"/>
+      <RecipeCard
+        v-for="(recipe, index) in recipes"
+        :key="index"
+        :recipedetails="recipe"
+        @update-favourites="updateMe"
+      />
     </div>
   </div>
 </template>
